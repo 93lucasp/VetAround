94db3b4fddb8638eb34db2c1163c0fdaba6aad9c
93lucasp <spezzanoluca@gmail.com>
2016-10-14 08:29:36 +0200

    added some comments

--- config/routes.js
+++ config/routes.js
@@ -1,21 +1,23 @@
-// Dependencies
+// Dependencies;
 var express 		  = require('express'),
 	app 			  = express(),
 	mongoose 		  = require('mongoose'),
 	bodyParser 		  = require('body-parser'),
 	methodOverride 	  = require('method-override'),
 	router 			  = express.Router(),
-	// Requiring the controlles in this page so then i can use them in the routes to say where to go in the server side;
-	placesController  = require('../controllers/places');
+	placesController  = require('../controllers/places'); // Requiring the controlles in this page so then i can use them in the routes to say where to go in the server side;
 
 // Differents routes;
+
 router.route('/').get(function(req, res){
   res.send('welcome');
 });
+
 // Routes for places page;
 router.route('/places')
 	.get(placesController.index) //calling the index function in the server side;
 	.post(placesController.create); //calling the create function in the server side;
+
 // Routes for places/id page;
 router.route('/places/:id')
 	.delete(placesController.destroy) //calling the destroy function in the server side;
--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,24 @@
+// CreatePlace function called on the button click;
 var createPlace = function(e) {
-  var place = $(e.target).serialize();
+  var place = $(e.target).serialize();  //The .serialize() method creates a text string in standard URL-encoded notation from the form (example namePlace=name of the place);
   $.post("/places", place)
-    // If received status 200 will work the function done 
+    // If received status 200 will work the function done;
    .done(function(res) {
      window.location.href = '/places';
     })
-   // else if received status 500 working the fail function
+   // else if received status 500 working the fail function;
    .fail(function(err) {
     window.location.href = '/places';
     console.log("Error", err);
     }); 
 };
-
+// DeletePlace function called on the button click;
 var deletePlace = function(place){
-  var id = $(place).data().id;
+  var id = $(place).data().id; // Getting the id from data-id in the button tag;
   $.ajax({
     url: '/places/' + id,
     type: 'DELETE',
+    // If receving status 200 run the succes function;
     success: function(res) {
       window.location.href = '/places';
     }
