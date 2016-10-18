12b59b2a4ea2c9bdf596fccde936d436bd603c6a
93lucasp <spezzanoluca@gmail.com>
2016-10-18 16:51:44 +0200

    added Referencing Data to PlaceSchema

--- models/user.js
+++ models/user.js
@@ -3,10 +3,11 @@ var mongoose 	= require('mongoose'),
 	bcrypt      = require('bcrypt');
 
 var UserSchema  = new Schema({
-	firstName: {type: String, required: true},
-	lastName: {type: String, required: true},
-	email: {type: String, required: true},
+	firstName: 		{type: String, required: true},
+	lastName: 		{type: String, required: true},
+	email: 			{type: String, required: true},
 	passwordDigest: {type: String, required: true},
+	createdAt: 		{type: Date, required: false, default: Date.now}
 });
 
 var User       = mongoose.model('User', UserSchema);
