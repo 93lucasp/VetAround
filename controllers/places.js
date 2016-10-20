9b224c6c4707bd741b45e42d65cc44e88e89e9be
93lucasp <spezzanoluca@gmail.com>
2016-10-20 19:34:27 +0200

    added query to show the edit only if the user is who created the place

--- controllers/places.js
+++ controllers/places.js
@@ -67,9 +67,63 @@ var placesController = {
   show: function(req, res) {
   	var id = req.params.id;
   	Place.findById(id, function(err, place){
-      
-  		res.render("places/show", {namePlace: place.namePlace, nameDoc: place.nameDoc, city: place.city, address: place.address, id: place.id, postedBy: place.postedBy});
+      req.currentUser(function() {
+          if (req.user) {
+        
+          var postedBy = JSON.stringify(place.postedBy[0]);
+          var userId = JSON.stringify(req.user._id);
+          console.log("posted by111•–•", typeof postedBy + postedBy);
+          // console.log("req user •••••••••••••", req.user);
+          console.log("posted by222•-•", typeof userId + userId);
+            if(postedBy == userId) {
+              place.canDelete = true;
+            } 
+            else {
+              place.canDelete = false;
+            }
+          }
+
+          
+            console.log("can delete?????:", place.canDelete);
+        res.render("places/show", {namePlace: place.namePlace, nameDoc: place.nameDoc, city: place.city, address: place.address, id: place.id, postedBy: place.postedBy, canDelete: place.canDelete});
+      });
+  		
   	});
+  },
+
+  index: function(req, res) {
+    Place.find({}, function(err, docs) {
+      if(err) returnError(err);
+      docs.forEach(function(place) {
+        User.findOne({_id: place.postedBy[0]}, function(err, user){
+          place.user = user;
+        });
+      });
+      // console.log("currentUser is: ", req.currentUser);
+      req.currentUser(function() {
+        docs.forEach(function(place) {
+
+          if (req.user) {
+          // console.log("posted by111•–•", typeof place.postedBy[0].id);
+          // console.log("req user •••••••••••••", req.user);
+          // console.log("posted by222•-•", typeof req.user._id.id);
+          // console.log("posted by333•-•", (place.postedBy[0] == req.user._id));
+          var placePostedBy = JSON.stringify(place.postedBy[0]);
+          var userId = JSON.stringify(req.user._id);
+            if(placePostedBy == userId) {
+              place.canDelete = true;
+            } 
+            else {
+              place.canDelete = false;
+            }
+          }
+
+          
+            console.log("can delete?????:", place.canDelete);
+        });
+      });
+      res.render("places/index", {veterinary: docs});
+    }).sort({createdAt: -1}); // With .sort I am going to order the result of the find for the latest creater;
   },
 
 
